3a18306ca84d8677d9b71a5f93483db873abdb70
Rdabbaghi <146320436+Rdabbaghi@users.noreply.github.com>
2023-10-22 10:00:32 +0330

    Add files via upload

---
+++ blink.ino
@@ -0,0 +1,16 @@
+int ledpin=13;
+int timer = 500;
+// In this section, we make the initial settings
+void setup() 
+{
+  pinMode(ledpin, OUTPUT);
+}
+
+// We write the part that we want to repeat continuously here
+void loop()
+ {
+  digitalWrite(ledpin, HIGH);  // turn the LED on (HIGH is the voltage level)
+  delay(timer);                      // wait for a second
+  digitalWrite(ledpin, LOW);   // turn the LED off (LOW is the voltage level)
+  delay(timer);                      // wait for a second
+}
