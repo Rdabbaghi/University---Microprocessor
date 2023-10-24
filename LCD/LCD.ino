35e815f4ef7e1c554eac8ac08e7c6a31c0622017
Rdabbaghi <20rdabaghi02@gmail.com>
2023-10-24 12:25:39 +0330

    Test and adjust lcd using potentiometer

--- LCD/LCD.ino
+++ LCD/LCD.ino
@@ -1,21 +1,21 @@
 #include <LiquidCrystal.h>
-const int rs=12,en=11,d4=5,d5=4,d6=3,d7=2;
-LiquidCrystal lcd (rs , en , d4 , d5 , d6 , d7 );
+const int rs=12,en=11,d4=5,d5=4,d6=3,d7=2;               // Definition of lcd bases
+LiquidCrystal lcd (rs , en , d4 , d5 , d6 , d7 );       // Definition of lcd bases
 void setup ()
 {
-  pinMode(A3,INPUT);     //
-  serial.begin(9600);      //
-  lcd.begin(16,2);         // 
-  lcd.print(" (: Hellow Friend :) ");
+  pinMode(A3,INPUT);                                 // Input port definition
+  serial.begin(9600);                               // Definition of series
+  lcd.begin(16,2);                                 // Definition of resolution
+  lcd.print(" (: Hellow Friend :) ");             // show the desired message
 
 }
 void loop()
 {
-  int val = analogRead(A3);
-  serial.print("POT Voltage :");
-  float d =val * 5 / 1023;
-  serial.println(d);
-  lcd.setCursor(0,1);
-  lcd.print(d);
+  int val = analogRead(A3);                  // Get the value from the defined base
+  serial.print("POT Voltage :");            // show the desired message
+  float d =val * 5 / 1023;                 // 
+  serial.println(d);                      // show the desired message
+  lcd.setCursor(0,1);                    // Setting the location of the posts
+  lcd.print(d);                         // show the desired message
 
 }
