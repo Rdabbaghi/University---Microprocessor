bed67f2094245e8cbbf058bd512ec3efab095ff7
Rdabbaghi <20rdabaghi02@gmail.com>
2023-10-24 09:46:46 +0330

    Create a code for the led to turn off and on repeatedly

--- blink/blink.ino
+++ blink/blink.ino
@@ -9,8 +9,8 @@ void setup()
 // We write the part that we want to repeat continuously here
 void loop()
  {
-  digitalWrite(ledpin, HIGH);  // turn the LED on (HIGH is the voltage level)
-  delay(timer);                      // wait for a second
-  digitalWrite(ledpin, LOW);   // turn the LED off (LOW is the voltage level)
-  delay(timer);                      // wait for a second
+  digitalWrite(ledpin, HIGH);          // turn the LED on (HIGH is the voltage level)
+  delay(timer);                       // wait for a second
+  digitalWrite(ledpin, LOW);         // turn the LED off (LOW is the voltage level)
+  delay(timer);                     // wait for a second
 }
