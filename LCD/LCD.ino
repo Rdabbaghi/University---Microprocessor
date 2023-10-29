defc3abb281f5c2e870c286d10b9a3b9f34d37e4
Rdabbaghi <20rdabaghi02@gmail.com>
2023-10-29 08:32:36 +0330

    Test and adjust LCD using potentiometer

--- LCD/LCD.ino
+++ LCD/LCD.ino
@@ -1,16 +1,16 @@
-#include <LiquidCrystal.h>
+#include <LiquidCrystal.h>                                // Library required for this project
 const int rs=12,en=11,d4=5,d5=4,d6=3,d7=2;               // Definition of lcd bases
 LiquidCrystal lcd (rs , en , d4 , d5 , d6 , d7 );       // Definition of lcd bases
-void setup ()
-{
+void setup ()                                          //
+{                                                     //
   pinMode(A3,INPUT);                                 // Input port definition
   serial.begin(9600);                               // Definition of series
   lcd.begin(16,2);                                 // Definition of resolution
   lcd.print(" (: Hellow Friend :) ");             // show the desired message
-
-}
-void loop()
-{
+                                                 //
+}                                               //
+void loop()                                    //
+{                                             //
   int val = analogRead(A3);                  // Get the value from the defined base
   serial.print("POT Voltage :");            // show the desired message
   float d =val * 5 / 1023;                 // 
