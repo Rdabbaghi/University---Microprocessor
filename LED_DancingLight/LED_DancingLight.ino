c57b3cbd00e86e7e1c0bfe548f55ab2ece334685
Rdabbaghi <146320436+Rdabbaghi@users.noreply.github.com>
2023-11-04 12:04:08 +0330

    Update LED_DancingLight.ino

--- LED_DancingLight/LED_DancingLight.ino
+++ LED_DancingLight/LED_DancingLight.ino
@@ -1,27 +1,22 @@
 // Define required variables
-int led1=1;
-int led2=2;
-int led3=3;
-int led4=4;
-int led5=5;
-int timer =500;
-
-
-
-void setup() {
-  for (int  pin=2; pin < 7 ; pin++)     // Identify pins as outputs
-  
-  pinMode(pin, OUTPUT);                 // Identify pins as outputs
-
-}
-
-void loop() {
-  for (int pin = 2 ; pin < 8 ; pin ++)
-  {
-    digitalWrite(pin,HIGH);       // Turn on the lights in sequence
-    delay(timer);                 // Wait for second 
-
-    digitalWrite(pin - 1 ,LOW);    // Turn off the lights in sequence
+int led1=1;                                             // Define required variables
+int led2=2;                                            // Define required variables
+int led3=3;                                           // Define required variables
+int led4=4;                                          // Define required variables
+int led5=5;                                         // Define required variables
+int timer =500;                                    // Define delay variable
+void setup()                                      //
+{                                                //
+  for (int  pin=2; pin < 7 ; pin++)             // Identify pins as output
+  pinMode(pin, OUTPUT);                        // Identify pins as outputs
+}                                             //
+void loop()                                  //
+{                                           //
+  for (int pin = 2 ; pin < 8 ; pin ++)     // Definition of the repetition loop for turning on and off the leds
+  {                                       //
+    digitalWrite(pin,HIGH);              // Turn on the lights in sequence
+    delay(timer);                       // Wait for second 
+    digitalWrite(pin - 1 ,LOW);        // Turn off the lights in sequence
   }
 
 }
