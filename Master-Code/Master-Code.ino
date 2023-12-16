e3350ec3ab051ef6a8b953dfdb884d258f34fd97
rdabbaghi <20rdabaghi02@gmail.com>
2023-12-16 17:21:32 +0330

    کد مستر جهت ارتباط سریال

---
+++ Master-Code/Master-Code.ino
@@ -0,0 +1,15 @@
+#include <Wire.h>
+
+short age = 0;
+void setup() {
+  Wire.begin();
+}
+
+void loop() {
+  Wire.beginTransmission(2);
+  Wire.write("Age is =");
+  Wire.write("age");
+  Wire.endTransmission();
+  delay(1000);
+}
+//کد مستر جهت ارتباط سریال 
